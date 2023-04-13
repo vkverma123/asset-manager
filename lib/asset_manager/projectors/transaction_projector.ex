90f3e2388f44740e198ea3b2507b691ebab1a3b4
unknown <vkverma.mnnit@gmail.com>
2023-04-13 16:31:31 +0700

    Adding-rolling-sum-logic

--- lib/asset_manager/projectors/transaction_projector.ex
+++ lib/asset_manager/projectors/transaction_projector.ex
@@ -4,16 +4,32 @@ defmodule AssetManager.Projectors.TransactionProjector do
     repo: AssetManager.Repo,
     name: "TransactionProjector"
 
-  alias Ecto.Multi
+  alias Ecto.{Changeset, Multi}
+  alias AssetManager.Repo
   alias AssetManager.ServiceAccounts.Events.Deposited
   alias AssetManager.Projections.DepositTransaction
 
   project(%Deposited{} = evt, _, fn multi ->
     multi
-    |> Multi.insert(:deposit_transaction, %DepositTransaction{
-      request_id: evt.request_id,
-      amount: evt.amount,
-      created_at: evt.created_at
-    })
+    |> Multi.run(:all, fn _repo, _changes ->
+      all =
+        Repo.get_by(DepositTransaction,
+          created_at: evt.created_at
+        ) ||
+          %DepositTransaction{
+            request_id: evt.request_id,
+            amount: Decimal.new(0),
+            created_at: evt.created_at
+          }
+
+      {:ok, all}
+    end)
+    |> Multi.insert_or_update(:all_updated, fn %{all: %DepositTransaction{} = all} ->
+      all
+      |> Changeset.change(
+        amount: Decimal.add(all.amount, evt.amount),
+        created_at: evt.created_at
+      )
+    end)
   end)
 end
